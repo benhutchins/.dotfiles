7db9e9adbb34d03c193f4ca9fc9abffaba061ed8
Benjamin Hutchins <ben@hutchins.co>
2015-11-03 11:12:34 -0500

    Adding NVM to node.sh

--- node.sh
+++ node.sh
@@ -1,3 +1,9 @@
 if [ -n `which node` ]; then
   NODE_PATH=/usr/local/lib/node_modules
 fi
+
+# Node Version Manager (nvm)
+if [ -e "/usr/local/opt/nvm/nvm.sh" ]; then
+  export NVM_DIR=~/.nvm
+  source /usr/local/opt/nvm/nvm.sh
+fi
