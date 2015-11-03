b7152392a1a3fc8f8cb0bc52ad34cc18a9ece70b
Benjamin Hutchins <ben@hutchins.co>
2015-11-03 10:46:37 -0500

    Removing JAVA_HOME env and disabling editor.sh which wanted to use sublime.

--- base.sh
+++ base.sh
@@ -1,7 +1,7 @@
 export DF_BASE_PATH=~/.dotfiles
 #export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
 #export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home"
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home"
+#export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home"
 
 source $DF_BASE_PATH/aliases.sh
 source $DF_BASE_PATH/autojump.sh
@@ -11,7 +11,7 @@ source $DF_BASE_PATH/bash_path.sh
 source $DF_BASE_PATH/bash_prompt.sh
 source $DF_BASE_PATH/control.sh
 source $DF_BASE_PATH/docker.sh
-source $DF_BASE_PATH/editor.sh
+#source $DF_BASE_PATH/editor.sh
 source $DF_BASE_PATH/git.sh
 source $DF_BASE_PATH/go.sh
 source $DF_BASE_PATH/heroku.sh
