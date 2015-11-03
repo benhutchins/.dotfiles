c45f8eb61d8161ac50520e8bfd728e3da57e4984
Benjamin Hutchins <ben@hutchins.co>
2015-11-03 10:45:50 -0500

    Replacing boot2docker environment variables with docker-machine's

--- docker.sh
+++ docker.sh
@@ -1,1 +1,4 @@
-export DOCKER_HOST=tcp://192.168.59.103:2375
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.100:2376"
+export DOCKER_CERT_PATH="$HOME/.docker/machine/machines/default"
+export DOCKER_MACHINE_NAME="default"
